467ed457e526eddb07196737be01b821f65af877
Aghogho <monorienaghogho@gmail.com>
2025-05-30 23:27:34 +0100

    update base url

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,7 +15,7 @@ const leagues = [
   { name: "BRA", endpoint: "bra" },
 ]
 
-const API_BASE = process.env.BASE_URL || "http://127.0.0.1:8000";
+const API_BASE = process.env.BASE_URL || "https://football-prediction-backend-wad9.onrender.com" // "http://127.0.0.1:8000";
 
 export default function Home() {
   const [selectedLeague, setSelectedLeague] = useState('mls');
