49b870ff9461f1e2d126ec67417b6bb8c4de61b6
Aghogho <monorienaghogho@gmail.com>
2025-06-24 05:36:57 +0100

    Mark BRA-B

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,7 +13,7 @@ type Prediction = {
 const leagues = [
   { name: "MLS", endpoint: "mls" },
   // { name: "BRA", endpoint: "bra" },
-  { name: "BRA-B", endpoint: "bra_b"},
+  { name: "BRA-B*", endpoint: "bra_b"},
   { name: "SWE-A", endpoint: "swe_a" },
   { name: "SWE", endpoint: "swe" },
   { name: "FIN", endpoint: "fin" },
