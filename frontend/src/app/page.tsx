06ba80161d2cbdea9b0185994f67a8ef2feb069b
Aghogho <monorienaghogho@gmail.com>
2025-07-02 14:45:41 +0100

    2nd June

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,13 +15,13 @@ const leagues = [
   // { name: "BRA", endpoint: "bra" },
   { name: "BRA-B*", endpoint: "bra_b"},
   { name: "SWE-A", endpoint: "swe_a" },
-  { name: "SWE", endpoint: "swe" },
+  // { name: "SWE", endpoint: "swe" },
   { name: "FIN*", endpoint: "fin" },
-  { name: "JPN", endpoint: "jpn" },
+  // { name: "JPN", endpoint: "jpn" },
   { name: "NOR", endpoint: "nor" },
-  // { name: "CHI", endpoint: "chi" },
-  { name: "CHN", endpoint: "chn" },
-  // { name: "USL", endpoint: "usl" },
+  { name: "CHI", endpoint: "chi" },
+  // { name: "CHN", endpoint: "chn" },
+  { name: "USL", endpoint: "usl" },
 ]
 
 const API_BASE = process.env.BASE_URL || "https://football-prediction-backend-wad9.onrender.com" // "http://127.0.0.1:8000";
